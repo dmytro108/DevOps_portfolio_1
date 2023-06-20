510ab95e471a9b30cdb760d099d9e348f76d65b3
dmytro108 <dmitry.fedunets@gmail.com>
2023-06-20 01:45:31 +0100

    update outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,11 @@
 output "public_ip" {
-  description = "Control node publick IP address"
+  description = "Web server publick IP address"
   value       = aws_eip.webserver_ip.public_ip
 }
-
+output "public_address" {
+  description = "Web server DNS"
+  value       = aws_eip.webserver_ip.public_dns
+}
 output "user" {
   value = aws_iam_user_login_profile.oversecured.user
 }
